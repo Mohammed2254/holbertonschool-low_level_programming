e8068117c6329cb134e73176306be92be21426b3
Mohammed2254 <m.a.alabduli@gmail.com>
2025-10-16 15:24:17 +0000

    f

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,18 +8,21 @@
 int _islower(int c)
 { int i;
 
+	int g;
+
 	for( i = 97;i >= 97 && i < 123;  i++)
 	{
 	if ( i == c )
 	{
-		return (1);
+		g = 1 ;
 	
 	} else if (i != c)
 	{
 		if (c == 122)
 		{
-		return (0);
+		g = 0;
 		}
 	}
-	}	
+	}
+return(g);	
 }
