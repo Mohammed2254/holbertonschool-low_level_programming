740381b50ade237326e9344cb14b584cd4ee9c28
Mohammed2254 <m.a.alabduli@gmail.com>
2025-10-16 09:54:38 +0000

    g

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the last digit of a random number
+ * and states whether it is greater than 5, less than 6 and not 0, or 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+for (int i = 97; i < 123; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
