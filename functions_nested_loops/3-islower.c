f577aa4a682749d4a9454efd7f66beb618d69641
Mohammed2254 <m.a.alabduli@gmail.com>
2025-10-16 15:32:54 +0000

    f

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -6,22 +6,9 @@
  * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c)
-{ int i;
-
-	int g;
-
-	for( i = 97;i >= 97 && i < 123;  i++)
-	{
-	if ( i == c )
-	{
-		g = 1 ;
-	
-	} else if (i != c)
-	{
-		g = 0;
-		
-		
-	}
-	}
-return(g);	
+{ 
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
