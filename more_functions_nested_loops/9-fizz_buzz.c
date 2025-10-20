72a46f94f37dc8f2fd3e7853da5b841e14350cd2
Mohammed2254 <m.a.alabduli@gmail.com>
2025-10-20 15:51:05 +0000

    f

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if (n % 5 == 0 && n % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 5 != 0 && n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0 && n % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+
+		printf("%d ", n);
+		}
+	}
+
+return (0);
+}
