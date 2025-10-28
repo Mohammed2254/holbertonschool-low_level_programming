651bc364198257ec3c0ed29cc4ecfc6cbb153798
m <m.a.alabduli@gmail.com>
2025-10-28 15:02:59 +0000

    g

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+
+int _putchar(char c);
+/**
  * _puts - prints a string followed by a new line
  * @str: pointer to the string to print
  *
