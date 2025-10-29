2f552167497d237ff43700ece913f40a77692043
Mohammed <m.a.alabduli@gmail.com>
2025-10-29 14:25:35 +0000

    g

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+	for (i  = length-1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
