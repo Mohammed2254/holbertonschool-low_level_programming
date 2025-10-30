63618e8f74d4fd5ed7c2eeaf1efcd409000c1bdb
Mohammed <m.a.alabduli@gmail.com>
2025-10-30 11:40:38 +0000

    g

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,11 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	for (; i <= n; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
 	printf("%d", a[i]);
+	if ( i != n -1)
+		printf(", ");
 	}
-	_putchar('\n');
+	printf("\n");
 }
