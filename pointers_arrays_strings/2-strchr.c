d2d36d51fa4f66be651e3ea130988deb74cfee89
mohammed <m.a.alabduli@gmail.com>
2025-11-01 18:01:16 +0000

    g

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strchr - Locates the first occurrence of the character c in the string s.
+ * The terminating null byte (\0) is considered part of the string.
+ * @s: The pointer to the string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: A pointer to the first occurrence of the character c in the string,
+ * or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, found = 0;
+
+	if (c == '\0')
+	{
+		return ("");
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			found = i;
+			break;
+		}
+		i++;
+	}
+	if (found)
+	{
+		return (&s[found]);
+	}
+	return (0);
+}
