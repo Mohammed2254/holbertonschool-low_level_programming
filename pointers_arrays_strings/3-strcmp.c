7220cc5bf23a86b129bd2ec00013fff979ec9dac
mohammed <m.a.alabduli@gmail.com>
2025-11-03 13:08:48 +0000

    osdkf

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (; s1[i] != '\0' && s2[i] != '\0';i++)
 	{
-		if ((s1[i] - s2[i]) != 0)
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		if (s1[i] - s2[i] != 0)
+		return (s1[i] - s2[i]);
 	}
-	return (0);
+return (0);
 }
