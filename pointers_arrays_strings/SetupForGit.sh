f30ae4dd5c229b333bd8d1247d13358ac80d78c8
mohammed <m.a.alabduli@gmail.com>
2025-11-03 11:10:16 +0000

    d

---
+++ pointers_arrays_strings/SetupForGit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global user.name "mohammed"
+git config --global user.email "m.a.alabduli@gmail.com"
+git config --global credential.helper store
+echo "Git username and email have been set to mohammed (m.a.alabduli@gmail.com) and credentials will be stored."
