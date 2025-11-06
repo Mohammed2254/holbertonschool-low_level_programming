ea0e43847d3a3dd264fbae739659e65bee3adbbc
mohammed <m.a.alabduli@gmail.com>
2025-11-06 09:45:46 +0000

    f

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,17 +4,16 @@
 * create_array - f chars and initializes it with a specific char
 * @size: The size of the array
 * @c: The character to fill the array with
-*
 * Return: Pointer to the created array, or NULL if size = 0 or if malloc fails
 */
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i = 0;
-
 if (size == 0)
 return (NULL);
-char *arr = malloc(sizeof(char) * size);
+
+arr = malloc(sizeof(char) * size);
 if (arr == NULL)
 return (NULL);
 
