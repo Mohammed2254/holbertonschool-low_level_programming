8625a784cbd04b1f4897cf590bd369f4711e0b40
mohammed <m.a.alabduli@gmail.com>
2025-11-08 12:08:50 +0000

    gdf

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,34 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+* alloc_grid - creates a 2D array of integers
+* @width: number of columns
+* @height: number of rows
+*
+* Return: pointer to the allocated 2D array, or NULL on failure
+* Each element of the grid is initialized to 0
+* If width or height is 0 or negative, returns NULL
+*/
 int **alloc_grid(int width, int height)
 {
-        int i;
-        int **array = NULL;
-        int j;
-        if (width <= 0 || height <= 0)
-                return (NULL);
+int i;
+int **array = NULL;
+int j;
+	if (width <= 0 || height <= 0)
+	return (NULL);
 
 	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
-return (NULL);
-                for (i = 0; i < height; i++)
-                {
-                        array[i] = malloc(sizeof(int) * width);
+	return (NULL);
+		for (i = 0; i < height; i++)
+		{
+			array[i] = malloc(sizeof(int) * width);
 			if (array[i] == NULL)
 			{
 				for (j = 0; j < i; j++)
-					free(array[j]);
-					free(array);
-				return (NULL);
+				{
+				free(array[j]);
+				}
+			free(array);
+			return (NULL);
 			}
-                }
-                i = 0;
-                while (i < height)
-                {
+		}
+		i = 0;
+			while (i < height)
+			{
 
-                        for ( j = 0; j < width; j++)
-                        array[i][j] = 0;
-                i++;
-                }
-        return (array);
+				for (j = 0; j < width; j++)
+				array[i][j] = 0;
+				i++;
+			}
+return (array);
 }
