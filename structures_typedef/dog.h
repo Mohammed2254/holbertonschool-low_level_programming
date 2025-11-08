c84ca76b4499901b237699b18a2ad54855ec8d9e
mohammed <m.a.alabduli@gmail.com>
2025-11-08 15:34:16 +0000

    g

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct Dog - Structure to store information about a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The name of the dog's owner
- */
-struct dog
+* struct dog - Structure to store information about a dog
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The name of the dog's owner
+* Description: This structure holds basic information about a dog.
+*/
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 #endif /* DOG_H */
