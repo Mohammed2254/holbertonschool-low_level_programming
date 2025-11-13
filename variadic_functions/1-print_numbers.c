f39f9b93c177775b2902231bff155a34adc2dd6d
Your Name <you@example.com>
2025-11-13 13:40:45 +0000

    f

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list par;
+
+va_start(par, n);
+	for (i = 0 ; i < n; i++)
+	{
+		printf("%d", va_arg(par, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%c", *separator);
+	}
+va_end(par);
+
+printf("\n");
+}
