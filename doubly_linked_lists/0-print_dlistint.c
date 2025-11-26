6e2ed1719df7b7124da9066b69f2eddf8ba3c559
Mohammed <m.a.alabduli@gmail.com>
2025-11-26 15:18:28 +0000

    d

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+size_t print_dlistint(const dlistint_t *h)
+{
+int count = 0;
+
+	while (n != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count =+ 1
+	}
+return (count)
+}
