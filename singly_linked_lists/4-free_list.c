2b23c4cf476efb5a58d4edf77682051407f56b99
Mohammed <m.a.alabduli@gmail.com>
2025-11-26 14:24:23 +0000

    g

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the linked list
+ *
+ * Description: traverses the linked list and frees each node's memory,
+ *              including the string stored in each node.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+	list_t *temp = NULL;
+
+	while (ptr != NULL)
+	{
+		temp = ptr->next;
+		free(ptr);
+			ptr = temp;
+	}
+
+}
