fe13f96daee98b677dcfedc79c5dcf6059eac5e5
mohammed <m.a.alabduli@gmail.com>
2025-12-31 14:08:18 +0000

    h

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,12 +2,6 @@
 /**
  * leet - Converts certain letters in a string to their "1337" (leet) equivalents.
  *
- * This function replaces specific alphabetic characters with corresponding
- * numeric characters based on a predefined mapping:
- *   a/A -> 4, e/E -> 3, o/O -> 0, t/T -> 7, l/L -> 1
- *
- * The conversion is done in-place, meaning the original string is modified.
- *
  * @s: Pointer to the string to be converted.
  *
  * Return: Pointer to the modified string.
